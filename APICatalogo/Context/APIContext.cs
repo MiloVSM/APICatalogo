50e71b4bd0e0a283ee0803f99d7c7a6c41e85983
Murilo Vieira <murilo1.0@outlook.com>
2024-03-03 07:00:33 -0300

    feat: add DB context and EF configuration

---
+++ APICatalogo/Context/APIContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class APIContext : DbContext
+    {
+        public APIContext (DbContextOptions<APIContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,9 +1,28 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Categories")]
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImageURL { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
     }
 }
