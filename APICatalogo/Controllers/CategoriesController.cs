65950c30325859dd724608baebb1d60d24b3c544
Murilo Vieira <murilo1.0@outlook.com>
2024-03-04 15:57:34 -0300

    feat: add all methods and endpoints to CategoriesController Adds endpoints for: - Get all categories - Get Category by Id - Get all categories and its products - POST a new category - PUT update a existing category - DELETE a existing category

---
+++ APICatalogo/Controllers/CategoriesController.cs
@@ -0,0 +1,82 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public CategoriesController(APIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("products")]
+        public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
+        {
+            return _context.Categories.Include(p => p.Products).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Category> Get(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound("No category was found...");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Category category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = category.CategoryId }, category);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category is null)
+                return NotFound("Category not found...");
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return Ok(category);
+        }
+    }
+}
